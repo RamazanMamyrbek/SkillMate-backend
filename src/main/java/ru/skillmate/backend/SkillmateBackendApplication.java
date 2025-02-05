852bc7193d0698a48c198ba56e254fa513149d97
RamazanMamyrbek <rama.mamirbek@gmail.com>
2025-02-05 16:27:52 +0500

    First init

---
+++ src/main/java/ru/skillmate/backend/SkillmateBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.skillmate.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillmateBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillmateBackendApplication.class, args);
+    }
+
+}
